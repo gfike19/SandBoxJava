2094769a8e9a74272d57091788ec7fe788c67ce5
Gail Fike <gfike19@gmail.com>
2018-09-10 18:15:24 -0500

    got desired results

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
 		System.out.println("Enter an item: ");
 		String userInput = input.nextLine();
 		input.close();
+		
 		String plu = createPlu(userInput);
 		System.out.println("The plu that was generated was: " + plu);
+		System.out.print("PLU length is: " + plu.length());
 	}
 	public static String createPlu (String input) {
 		String plu = "";
-		
-		while (plu.length() < 12) {
-			
+					
 			for (int i = 0; i < input.length(); i++) {
 				int temp = (int)input.charAt(i);
-				plu += temp;
+				if (plu.length() < 12) {
+					plu += temp;
+				}
 			}
-		}
+			
+			if (plu.length() > 12) {
+				plu = plu.substring(0, 12);
+			}
 		
 		return plu;
 	}

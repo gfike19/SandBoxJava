81fe2d8ffb8499ac4bae009895f72d06b7704a2c
gfike19@gmail.com <Vaughn*573>
2019-08-08 13:59:05 -0500

    added todo

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import java.io.BufferedReader;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-
+/**TODO
+ * make list of numbers and write to text file they can be
+ * read back in and prime number test from Bayer can be run
+ * **/
     public static void main(String[] args) {
+            String thisLine = "";
+        BufferedReader br = new BufferedReader(System.in);
 
+        }
+
+        public static void genRandomNum (){
+            SecureRandom srg = new SecureRandom();
+            byte[] rBytes = new byte[128];
+            int r = srg.nextInt();
         }
     }
 

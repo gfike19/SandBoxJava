1aedfb83c82728af6dbbcdb2c3e5e24c754696ce
gfike19@gmail.com <Vaughn*573>
2019-08-09 13:59:39 -0500

    am able to create file, working on reading it in, and finding prime numbers

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,72 @@
-import java.io.BufferedReader;
+import java.io.*;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-/**TODO
- * make list of numbers and write to text file they can be
- * read back in and prime number test from Bayer can be run
- * **/
-    public static void main(String[] args) {
-            String thisLine = "";
-        BufferedReader br = new BufferedReader(System.in);
+    public static void main(String[] args) throws IOException {
+        genFile();
 
+        File file = new File("C:\\Users\\aerot\\coding\\SandBoxJava\\text.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+
+        while((st = br.readLine()) != null){
+            System.out.println(st);
+
+            int num = Integer.parseInt(st);
+
+            /*
+            TODO need to figure how to determine prime number condition
+             */
+
+            for(int i = 2; i < num - 1; i++) {
+                int count = 0;
+
+                if (num / i )
+            }
+        }
         }
 
-        public static void genRandomNum (){
+    public static int genRandomNum () {
+        int r = 0;
+        try {
             SecureRandom srg = new SecureRandom();
-            byte[] rBytes = new byte[128];
-            int r = srg.nextInt();
+            byte[] randomBytes = new byte[128];
+            r = srg.nextInt(25);
+            return r;
+        }catch(Exception e){
+            System.out.println(e.toString());
         }
+        return r;
+    }
+
+    public static void genFile () {
+
+        ArrayList<String> arr = new ArrayList<String>();
+
+        for(int i = 0; i < 3; i++) {
+            int r = genRandomNum();
+            arr.add("" + r);
+        }
+
+        try{
+            PrintWriter pw = new PrintWriter("text.txt", "UTF-8");
+
+            for(String s : arr){
+                pw.write(s + "\n");
+            }
+
+            pw.close();
+
+        }catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+
     }
 
 

2b34dc83324f6e07fa097ebc1685dfca445c8f85
gfike19 <gfike19@gmail.com>
2020-08-04 19:09:26 -0500

    did fizzbuzz exercise

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,33 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        // 3 - fizz
+        // 5 - buzz
+        // 3 and 5 - fizzbuzz
+        // neither i
 
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = in.nextInt();
+        for(int i = 1; i <= num; i++) {
+            String disp = "";
+            if(i % 3 == 0) {
+                disp += "fizz";
+            }
+            if(i % 5 == 0) {
+                disp += "buzz";
+            }
+
+            if(i % 5 != 0 && i % 3 != 0){
+                disp += i;
+            }
+            System.out.println(disp);
+        }
         }
     }
 
